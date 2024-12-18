dda4df2ee90e8effc5e3a9abaaa372e749862466
Lucky Goyal <luckygoyal1@districtd.in>
2024-12-18 16:54:13 +0530

    mistakes layout fix

--- frontend/app/mistakes/page.tsx
+++ frontend/app/mistakes/page.tsx
@@ -7,7 +7,7 @@ const Page: React.FC = () => {
     return (
         <div className="flex h-screen bg-gray-100">
             <Sidebar />
-            <main className="flex-1 p-4 sm:ml-48 overflow-y-auto">
+            <main className="flex-1 p-4 md:ml-48 overflow-y-auto">
                 <MyMistakes />
             </main>
         </div>
